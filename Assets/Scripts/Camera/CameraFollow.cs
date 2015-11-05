6a099e4ff23245a4bc80615393845f58c965129d
Anas <anas2006101@gmail.com>
2015-11-05 15:56:37 +0300

    Summar
    y

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+    public float smoothing = 5f;
+
+	Vector3 offest;
+
+	void Start(){
+		offest = transform.position - target.position;
+	}
+	void FixedUpdate(){
+		Vector3 targetCamPos = target.position + offest;
+		transform.position =Vector3.Lerp (transform.position , targetCamPos,smoothing * Time.deltaTime);
+	}
+}
